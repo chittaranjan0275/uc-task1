f7f59ee4228cf37786e0510a5b55a21a0bf5f607
chittaranjan0275 <73305182+chittaranjan0275@users.noreply.github.com>
2022-01-12 20:07:00 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,5 +117,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'chittaranjan0275@gmail.com'
-EMAIL_HOST_PASSWORD = 'rathodcv$/258'
+EMAIL_HOST_USER = 'abccd@gmail.com'
+EMAIL_HOST_PASSWORD = '*****'
